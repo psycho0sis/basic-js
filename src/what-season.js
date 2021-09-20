b7e5fb58c2c3a70416147ab68b242b903b767ae4
Ilona <ilonakot92@gmail.com>
2021-09-20 08:04:58 +0300

    feat: add a what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -13,9 +13,18 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function getSeason(date) {
   if (date === undefined) return 'Unable to determine the time of year!';
-  // if (date instanceof Date) {
-  //   return true;
-  // } else {
-  //   return 'Invalid date!';
-  // }
+  if (date instanceof Date) {
+    let month = date.getMonth();
+    if (month === 11 ||month === 00 || month === 01) {
+      return "winter";
+    } else if (month === 02 || month === 03 || month === 04){
+      return "spring";
+    } else if (month === 05 || month === 06 || month === 07){
+      return "summer";
+    } else if (month === 08 || month === 09 || month === 10){
+      return "autumn";
+    } 
+  } else {
+    return 'Invalid date!';
+  }
 }
