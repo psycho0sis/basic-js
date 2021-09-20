289e3e25a6619c3819d531ddbf435e797c771d9b
Ilona <ilonakot92@gmail.com>
2021-09-20 08:25:43 +0300

    mend
    feat: add what a season solution

--- src/what-season.js
+++ src/what-season.js
@@ -15,16 +15,16 @@ export default function getSeason(date) {
   if (date === undefined) return 'Unable to determine the time of year!';
   if (date instanceof Date) {
     let month = date.getMonth();
-    if (month === 11 ||month === 00 || month === 01) {
+    if (month === 11 ||month === 0 || month === 1) {
       return "winter";
-    } else if (month === 02 || month === 03 || month === 04){
+    } else if (month === 2 || month === 3 || month === 4){
       return "spring";
-    } else if (month === 05 || month === 06 || month === 07){
+    } else if (month === 5 || month === 6 || month === 7){
       return "summer";
-    } else if (month === 08 || month === 09 || month === 10){
+    } else if (month === 8 || month === 9 || month === 10){
       return "autumn";
     } 
   } else {
-    return 'Invalid date!';
+    throw new Error("Invalid date!");
   }
 }
