943bc6f6060652da538b4d9dac548c663060b5b9
Ilona <ilonakot92@gmail.com>
2021-10-04 22:16:49 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+    const {repeatTimes, separator = "+", addition = "", additionRepeatTimes, additionSeparator = ""} = options;
+  let mediumResult = str.toString() + (`${addition.toString()}${additionSeparator}`.repeat(additionRepeatTimes));
+  console.log(mediumResult)
+  let i = 0;
+  let result = "";
+  let index = 0;
+  while (repeatTimes > i){
+    if(index === repeatTimes - 1){
+      return result += mediumResult;
+    }
+    i++;
+    index++;
+    result += mediumResult + separator;
+    
+  }
+  return result;
 }
